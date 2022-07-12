daa4105b2ba369a0e0e3c9b1f793867fa5309d42
nicswebdev <nicholaspratama94s@gmail.com>
2022-07-12 17:07:39 +0800

    change delete return

--- routes/packages.js
+++ routes/packages.js
@@ -1,84 +1,84 @@
 const Packages = require("../models/Packages");
 const {
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
 } = require("./verifyToken");
 
 const router = require("express").Router();
 
 //Create
 router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newPackages = new Packages(req.body);
-  try {
-    const savedPackages = await newPackages.save();
+    const newPackages = new Packages(req.body);
+    try {
+        const savedPackages = await newPackages.save();
 
-    res.status(200).json(savedPackages);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+        res.status(200).json(savedPackages);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 //Update
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const updatedPackages = await Packages.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
+    try {
+        const updatedPackages = await Packages.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
 
-    res.status(200).json(updatedPackages);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+        res.status(200).json(updatedPackages);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 // //Delete
 router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    await Packages.findByIdAndDelete(req.params.id);
+    try {
+        await Packages.findByIdAndDelete(req.params.id);
 
-    res.status(200).json("Packages has been deleted!");
-  } catch (error) {
-    res.status(500).json(error);
-  }
+        res.status(200).json({ id: req.params.id });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 // //Get Packages
 router.get("/find/:id", async (req, res) => {
-  try {
-    const packages = await Packages.findById(req.params.id);
+    try {
+        const packages = await Packages.findById(req.params.id);
 
-    res.status(200).json(packages);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+        res.status(200).json(packages);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 // //Get All Packages
 router.get("/", async (req, res) => {
-  const qNew = req.query.new;
-  const qDeal = req.query.deal;
+    const qNew = req.query.new;
+    const qDeal = req.query.deal;
 
-  try {
-    let packages;
+    try {
+        let packages;
 
-    if (qNew) {
-      packages = await Packages.find().sort({ createdAt: -1 }).limit(5);
-    } else if (qDeal) {
-      packages = await Packages.find({
-        dealId: qDeal,
-      });
-    } else {
-      packages = await Packages.find();
+        if (qNew) {
+            packages = await Packages.find().sort({ createdAt: -1 }).limit(5);
+        } else if (qDeal) {
+            packages = await Packages.find({
+                dealId: qDeal,
+            });
+        } else {
+            packages = await Packages.find();
+        }
+
+        res.status(200).json(packages);
+    } catch (error) {
+        res.status(500).json(error);
     }
-
-    res.status(200).json(packages);
-  } catch (error) {
-    res.status(500).json(error);
-  }
 });
 
 module.exports = router;
